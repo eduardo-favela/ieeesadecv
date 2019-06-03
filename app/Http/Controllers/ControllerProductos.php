de446f72c5f4b00578c6e29ea3138cdf34bb1fab
Eduardo Favela <arturo99favela@gmail.com>
2019-06-03 17:57:03 -0500

    actualización 6

--- app/Http/Controllers/ControllerProductos.php
+++ app/Http/Controllers/ControllerProductos.php
@@ -10,8 +10,4 @@ class ControllerProductos extends Controller
     function verinicio(){
         return view('Inicio');
     }
-    function actualizar(){
-        $process = new Process('git pull');
-        $process->run();
-    }
 }
