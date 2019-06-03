43b2df8c192b7e2b5594d672658f18ed55e38f3f
Eduardo Favela <arturo99favela@gmail.com>
2019-06-03 16:41:00 -0500

    actualización

--- app/Http/Controllers/ControllerProductos.php
+++ app/Http/Controllers/ControllerProductos.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 
 class ControllerProductos extends Controller
 {
@@ -10,7 +11,7 @@ class ControllerProductos extends Controller
         return view('Inicio');
     }
     function actualizar(){
-        $process = new Process('');
+        $process = new Process(URL("actualizador.sh"));
         $process->run();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@
 */
 
 Route::get('/', 'ControllerProductos@verinicio');
+Route::get('/', 'ControllerProductos@actualizar');
