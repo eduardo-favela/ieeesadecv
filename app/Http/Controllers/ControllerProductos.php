6da685792d544f607277963e754ecee11f61fc37
Eduardo Favela <arturo99favela@gmail.com>
2019-06-03 16:45:34 -0500

    actualización4

--- app/Http/Controllers/ControllerProductos.php
+++ app/Http/Controllers/ControllerProductos.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
-
+use Symfony\Component\Process\Process;
 class ControllerProductos extends Controller
 {
     function verinicio(){
