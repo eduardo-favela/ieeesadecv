9490656bfc27e13e6ebebd63978b63c2b77343bf
Eduardo Favela <arturo99favela@gmail.com>
2019-06-06 08:47:04 -0500

    asdfghjklñ

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -21,7 +21,7 @@ class DeployController extends Controller
 //            });
 //        }
         $output=array();
-        exec('git pull 2>&1',$output);
+        exec('cd/home3/ieeesade/ieeesadecv&&git pull 2>&1',$output);
         var_dump($output);
     }
 }
