fde3b8a71060552a79c05fd957f0a6fb64b173d3
Eduardo Favela <arturo99favela@gmail.com>
2019-07-11 17:26:05 -0500

    Agregué configuración del background

--- resources/views/Inicio.blade.php
+++ resources/views/Inicio.blade.php
@@ -5,12 +5,18 @@
     @parent
 @show
 @section('Contenido')
-<div class="col-md-12" style="background-image:url('/images/dorada.jpeg');
+<div class="col-md-12" style="background-image:url('/images/doradas.jpg');
  background-repeat: no-repeat;
         background-position: center;
         background-attachment: fixed;
         background-size: cover;
-height:500px">
+height:500px; text-align: center">
+    <div style="padding-top: 240px;display: inline-block">
+        <div class="col-md-12" style="text-align: center"><p style="color: #FFFFFF;text-align:center;
+        font-size:80px; letter-spacing: 50px;
+        padding-left: 50px;
+        font-family: 'Century Gothic';">Bienvenido</p></div>
+    </div>
 </div>
 <br><br><br><br><br><br>
     <h5>hola</h5>
